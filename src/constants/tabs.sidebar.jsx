73973aa3b195317a40ae41a642b99986ff0f3c6c
rin-sama17 <okumura.rin.samaa@gmail.com>
2023-06-24 18:51:32 +0330

    refactor

--- src/constants/tabs.sidebar.jsx
+++ src/constants/tabs.sidebar.jsx
@@ -11,7 +11,7 @@ export const tabsData = [
   { text: 'خانه', to: '/', icon: <Home /> },
   { text: 'فروشگاه', to: '/products', icon: <Store /> },
   { text: 'وبلاگ', to: '/posts', icon: <Book /> },
-  { text: 'نمایندگی ها', to: '/contracts', icon: <Apartment /> },
+  { text: 'نمایندگی ها', to: '/agencies', icon: <Apartment /> },
   { text: 'تماس با ما', to: '/contact-us', icon: <SupportAgentOutlined /> },
   { text: 'درباره ما', to: '/about-us', icon: <InfoOutlined /> },
 ]
