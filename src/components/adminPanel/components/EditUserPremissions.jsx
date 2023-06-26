de7fbfb6f6fc039f75fe7c1d171292b81cf6a285
rin-sama17 <okumura.rin.samaa@gmail.com>
2023-06-26 21:23:11 +0330

    add premission to user

---
+++ src/components/adminPanel/components/EditUserPremissions.jsx
@@ -0,0 +1,92 @@
+import { BookmarkAddRounded } from '@mui/icons-material'
+import { GridActionsCellItem } from '@mui/x-data-grid'
+import { useFormik } from 'formik'
+import { useEffect, useMemo, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectAllPremissions } from '../../../reducers/premissionSlice'
+
+import {
+  editUser,
+  premissionIdAdded,
+  premissionIdDeleted,
+  premissionIdsCleared,
+  premissionsIdFinded,
+  selectPremission_id,
+} from '../../../reducers/userSlice'
+import { CustomModal, CustomForm } from '../../common'
+import { selectLang } from '../../../reducers/langSlice'
+
+const handleCheck = (dispatch, premissionId) => (e) => {
+  if (e.target.checked === true) {
+    dispatch(premissionIdAdded(premissionId))
+  } else {
+    dispatch(premissionIdDeleted(premissionId))
+  }
+}
+
+const EditUserPremissions = ({ user }) => {
+  const [open, setOpen] = useState(false)
+  const dispatch = useDispatch()
+
+  const userPremissions = useSelector(selectPremission_id)
+  const allPremissions = useSelector(selectAllPremissions)
+  const lang = useSelector(selectLang)
+
+  useEffect(() => {
+    if (open) {
+      dispatch(premissionsIdFinded(user.premissions))
+    } else {
+      dispatch(premissionIdsCleared())
+    }
+  }, [open])
+
+  const formik = useFormik({
+    initialValues: user,
+    onSubmit: (values, { resetForm }) => {
+      const newUser = {
+        ...values,
+        premissions: userPremissions,
+        lang,
+      }
+      console.log(newUser)
+      dispatch(editUser({ values: newUser, setOpen, resetForm }))
+    },
+  })
+  const extraFields = useMemo(() => {
+    const premissionFields = allPremissions?.map((premission) => ({
+      xs: 3,
+      checkbox: true,
+      formik,
+      name: premission.name,
+      customLabel: premission.name,
+      defaultChecked: userPremissions.includes(premission.id),
+      onChange: handleCheck(dispatch, premission.id),
+    }))
+    return [
+      ...premissionFields,
+      {
+        submit: true,
+        customLabel: 'ثبت',
+      },
+    ]
+  }, [userPremissions, dispatch, open])
+  return (
+    <>
+      <GridActionsCellItem
+        icon={<BookmarkAddRounded />}
+        color="success"
+        onClick={() => setOpen(true)}
+      />
+      <CustomModal open={open} setOpen={setOpen}>
+        <CustomForm
+          label="ویرایش دسترسی"
+          formik={formik}
+          extraFields={extraFields}
+          color="info"
+        />
+      </CustomModal>
+    </>
+  )
+}
+
+export default EditUserPremissions
