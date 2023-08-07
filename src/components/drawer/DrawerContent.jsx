6f428125db512146ef5d826330dd353e6ffdfa80
rin-sama17 <okumura.rin.samaa@gmail.com>
2023-08-07 20:27:34 +0330

    refactor

--- src/components/drawer/DrawerContent.jsx
+++ src/components/drawer/DrawerContent.jsx
@@ -1,12 +1,9 @@
 import { Divider, Box, Typography } from '@mui/material'
-import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
 import { DrawerItems } from '.'
 
 const DrawerContent = ({ setOpen }) => {
-  const { t } = useTranslation()
-
   return (
     <Box
       sx={{
@@ -17,16 +14,6 @@ const DrawerContent = ({ setOpen }) => {
         height: 1,
       }}
     >
-      <Typography
-        component={Link}
-        to="/"
-        variant="h5"
-        sx={{ ml: 1, color: 'title.dark' }}
-      >
-        {t('فرش لبخند')}
-      </Typography>
-      <Divider sx={{ color: 'bgcolor.dark', my: 2 }} />
-
       <DrawerItems setOpen={setOpen} />
     </Box>
   )
